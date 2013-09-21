a9bc0204826b9fde839dcc09d2558be63eb93ee3
eguitarz <dalema22@gmail.com>
2013-09-21 20:24:09 +0800

    builds

--- builds/simple-select.js
+++ builds/simple-select.js
@@ -1,4 +1,14 @@
 (function() {
-
+  $.fn.select = function(options) {
+    var list, ul;
+    list = options.list || {
+      'Not specified': null
+    };
+    ul = $('<ul>');
+    Object.keys(list).forEach(function(el) {
+      return ul.append("<li data-value=\"" + list[el] + "\">" + el + "</li>");
+    });
+    return this.html(ul);
+  };
 
 }).call(this);
